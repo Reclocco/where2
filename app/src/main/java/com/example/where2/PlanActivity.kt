ce74a0626d9f227d61b1e236367fcadca0003482
Michał Reclik <mrecl@dolby.com>
2021-11-16 12:30:12 +0100

    places

--- app/src/main/java/com/example/where2/PlanActivity.kt
+++ app/src/main/java/com/example/where2/PlanActivity.kt
@@ -1,6 +1,7 @@
 package com.example.where2
 
 import android.content.ContentValues.TAG
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,9 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
 import java.util.*
 
 class PlanActivity : AppCompatActivity() {
@@ -44,6 +48,26 @@ class PlanActivity : AppCompatActivity() {
             }
         })
 
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment2 =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment2)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment2.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment2.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onError(p0: Status) {
+                null
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+        })
+
 
         val spinner: Spinner = findViewById(R.id.spinner)
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -57,5 +81,18 @@ class PlanActivity : AppCompatActivity() {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+    }
+
+    fun makeApiCall(location: Location){
+        //TODO ADD PARAMETRIZED URL FOR PLACES AND MAKE IT WORK. take care of yourself.
+        val request = Request.Builder().url("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=1500&type=restaurant&key=YOUR_API_KEY")
+            .build()
+
+        val response = OkHttpClient().newCall(request).execute().body?.string()
+        val jsonObject = JSONObject(response) // This will make the json below as an object for you
+
+        // You can access all the attributes , nested ones using JSONArray and JSONObject here
+
+
     }
 }
